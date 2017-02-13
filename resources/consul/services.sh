a6131e4575aa03939233f341fabf0022e0443c08
FGtatsuro <fujiistorage@gmail.com>
2017-02-13 14:23:21 +0900

    Support develop mode of Consul/Nomad.

--- resources/consul/services.sh
+++ resources/consul/services.sh
@@ -2,10 +2,14 @@
 
 CONSUL_DATADIR=${CONSUL_DATADIR:-/tmp/consul}
 CONSUL_CONFIGDIR=${CONSUL_CONFIGDIR:-/etc/consul.d}
+CONSUL_BIND_ADDRESS=${CONSUL_BIND_ADDRESS:-127.0.0.1}
 OPTS="-node=${CONSUL_NODE_NAME} -bind=${CONSUL_BIND_ADDRESS} -client=${CONSUL_BIND_ADDRESS} -data-dir=${CONSUL_DATADIR} -config-dir=${CONSUL_CONFIGDIR}"
 if [ "$1" == "server" ]
 then
     OPTS="${OPTS} -server -bootstrap-expect=${CONSUL_SERVER_NUM}"
+elif [ "$1" == "dev" ]
+then
+    OPTS="${OPTS} -dev"
 fi
 if [ -n "${CONSUL_JOIN_ADDRESS}" ]
 then

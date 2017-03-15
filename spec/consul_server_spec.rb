33f2da7ac6b1242386ceaf651936379386347b77
FGtatsuro <fujiistorage@gmail.com>
2017-03-15 19:39:49 +0900

    Containers doesn't have commands related to capabilities.

--- spec/consul_server_spec.rb
+++ spec/consul_server_spec.rb
@@ -5,7 +5,9 @@ describe file("/etc/consul.d/consul_common.json") do
   its(:content) { should match /"server": true/ }
 end
 
-describe command('getcap /usr/local/bin/consul') do
-  its(:exit_status) { should eq 0 }
-  its(:stdout) { should match /#{Regexp.escape('/usr/local/bin/consul = cap_net_bind_service+ep')}/ }
+if ENV['TARGET_BACKEND'] == 'vagrant' then
+  describe command('getcap /usr/local/bin/consul') do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match /#{Regexp.escape('/usr/local/bin/consul = cap_net_bind_service+ep')}/ }
+  end
 end
